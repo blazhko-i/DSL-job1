fdc28d58e6ff4a7b371987d4afd0c8c023b8a299
blazhko-i <ilya2000269ilya5@gmail.com>
2021-09-20 15:49:06 +0300

    Add the final version of the project

--- main.py
+++ main.py
@@ -11,25 +11,20 @@ def main():
     text = get_text()
     whitespaces = [' ', '\n', '\t']
 
-    new_text = []  # This list contains our text without separator characters
-    buffer = 1  # Temporary variable
-    buf = ''  # Temporary variable to store the word
+    new_text = []  # This list contains our text without separator characters. Each element of the list
+    # is equal to the word
+    buffer = ''  # Temporary variable to store the word
     for symbol in text:
         """If the 'buffer' is equal to 0 therefore the program skips this symbol and changes 'buffer' to 1.
         Otherwise, the program adds the symbol to 'new_text'"""
-        for item in whitespaces:
-            if symbol == item:
-                buffer = 0
-                break
-        if buffer == 0:
-            if len(buf) != 0:  # If the text contains 2 spaces for example
-                new_text.append(buf)
-                buf = ''  # Remove the current word
-            buffer = 1
-            continue
+        if symbol not in whitespaces:
+            buffer += symbol
         else:
-            buf += symbol
-    new_text.append(buf)  # Add the last word
+            if len(buffer) != 0:  # If the text contains 2 spaces for example
+                new_text.append(buffer)
+                buffer = ''  # Remove the current word
+
+    new_text.append(buffer)  # Add the last word
     print(new_text)
 
 
